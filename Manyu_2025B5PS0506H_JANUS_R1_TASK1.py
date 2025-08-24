3527fb14bef8d15136eae391f896cec617688370
Bakasura18 <f20250506@hyderabad.bits-pilani.ac.in>
2025-08-24 20:14:51 +0530

    Manyu_2025B5PS0506H_JANUS_R1_TASK1.py

--- Manyu_2025B5PS0506H_JANUS_R1_TASK1.py
+++ Manyu_2025B5PS0506H_JANUS_R1_TASK1.py
@@ -14,36 +14,36 @@ altitudes_smooth = altitudes.rolling(window=3, center=True).mean()  # takes mean
 altitudes_smooth = altitudes_smooth.fillna(method="bfill").fillna(method="ffill")  # filling NaN values at edges
 
 velocities = altitudes_smooth.diff().fillna(0)  # Calculate velocities as the difference between consecutive altitudes  
-x = range(len(altitudes))  # creates a sequence of 0,1,2,3.. till length of altitudes-1
+x = range(len(altitudes))  # creates a sequence of 0,1,2,3.. till length of altitudes-1 (its a range object)
 
-# --- Animation Setup ---
-fig, ax1 = plt.subplots()
+# Animation part
+fig, ax1 = plt.subplots()#to create 2 objects, fig for the canvas and ax1 for the axes.
 
 # defining a y axis - Altitude axis
-ax1.set_xlim(0, len(altitudes))
+ax1.set_xlim(0, len(altitudes)) #setting x axis limits 
 ax1.set_ylim(min(altitudes.min(), altitudes_smooth.min()) - 5,
-             max(altitudes.max(), altitudes_smooth.max()) + 5)
+             max(altitudes.max(), altitudes_smooth.max()) + 5) #so that graph doesnt touch the bottom, adding a margin of 5
 
-raw_line, = ax1.plot([], [], 'bo-', markersize=2, alpha=0.5, label="Raw Altitude")
-smooth_line, = ax1.plot([], [], 'ro-', markersize=2, label="Smoothed Altitude")
+raw_line, = ax1.plot([], [], 'bo-', markersize=2, alpha=0.5, label="Raw Altitude")#a Line2D object for raw altitudes
+smooth_line, = ax1.plot([], [], 'ro-', markersize=2, label="Smoothed Altitude")#a Line2D object for smoothened altitudes
 
 ax1.set_xlabel("Time (s)")
 ax1.set_ylabel("Altitude (m)", color='black')
-ax1.grid(True)
+ax1.grid(True)#turn on  gridlines
 
 # defining another y axis - Velocity axis 
-ax2 = ax1.twinx()
-ax2.set_ylim(velocities.min() - 1, velocities.max() + 1)
-vel_line, = ax2.plot([], [], 'g--', linewidth=1.5, label="Vertical Velocity (m/s)")  # velocity line in green dashed
+ax2 = ax1.twinx()#to make a twin y axis that shares the same x axis.
+ax2.set_ylim(velocities.min() - 1, velocities.max() + 1)#again to add some margin 
+vel_line, = ax2.plot([], [], 'g--', linewidth=1.5, label="Vertical Velocity (m/s)")  # the comma is because it returns a tuple.
 ax2.set_ylabel("Velocity (m/s)", color='green')
 
 
-lines = [raw_line, smooth_line, vel_line]
-labels = [l.get_label() for l in lines]
-ax1.legend(lines, labels, loc="upper left", frameon=True)
+lines = [raw_line, smooth_line, vel_line]  #maing a python list to have all three line2D objects
+labels = [l.get_label() for l in lines] #to gather all 3 labels into  a list
+ax1.legend(lines, labels, loc="upper left", frameon=True)# to put the legend at the top left in a box
 
-# function to initialise
-def init():
+# function to initialise - to execute before the first frame (can be omitted though i.e. its optional)
+def init(): 
     raw_line.set_data([], [])
     smooth_line.set_data([], [])
     vel_line.set_data([], [])
@@ -56,8 +56,15 @@ def update(frame):
     vel_line.set_data(x[:frame], velocities[:frame])
     return raw_line, smooth_line, vel_line
 
-# 1 frame = 1 second
+
+
 ani = animation.FuncAnimation(fig, update, frames=len(altitudes),
                               init_func=init, blit=True, interval=1000, repeat=False)
-
+#calls the function update at each frame,
+#frames=len(altitudes) : this passes values from [0,1,2,3...len(altitudes)] to the update function, i.e. the source of data to pass to the update function with each frame
+#init_func=init : to execute before the first frame. 
+# blit=True : to turn on blitting so that only the part that changed is updated with each frame and not the whole stuff
+#interval=1000 : to display each new frame after 1000 milli seconds
+#repeat=False : to not repeat the animation after its over.
 plt.show()
+
